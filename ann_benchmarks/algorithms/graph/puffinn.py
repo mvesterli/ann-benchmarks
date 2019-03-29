25e7cc0a5ef91b41816dcc74c8e6c37ed8da9fc1
Michael Vesterli <michael@vesterli.dk>
2019-03-29 16:09:56 +0100

    Add plain lsh graph algorithm

---
+++ ann_benchmarks/algorithms/graph/puffinn.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from ann_benchmarks.algorithms.base import BaseANN
+from _puffinnwrapper import build_graph
+
+class PlainLSH(BaseANN):
+    def __init__(self, metric, params):
+        self._metric = metric
+        self._hash_function = params['hash_function']
+        self._recall = params['recall']
+        self._hash_length = params['hash_length']
+
+    def fit(self, X):
+        self._graph = build_graph(
+            self._metric,
+            X,
+            self._count,
+            method = 'plain',
+            hash_function = self._hash_function,
+            recall = self._recall,
+            hash_length = self._hash_length)
+
+    def query(self, idx, n):
+        return self._graph[idx]
+
+    def builds_graph(self):
+        return True
+
+    def set_count(self, count):
+        self._count = count
+
+    def __str__(self):
+        return 'PlainLSH(hash=%s, hash_length=%d, recall=%.2f)' % (self._hash_function, self._hash_length, self._recall)
