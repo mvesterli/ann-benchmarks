31596d10d29933937971b2d30e7a35f7569ecba4
Michael Vesterli <michael@vesterli.dk>
2019-03-31 08:53:29 +0200

    Add ProjectionLSH to graph algorithms

--- ann_benchmarks/algorithms/graph/puffinn.py
+++ ann_benchmarks/algorithms/graph/puffinn.py
@@ -30,3 +30,36 @@ class PlainLSH(BaseANN):
 
     def __str__(self):
         return 'PlainLSH(hash=%s, hash_length=%d, recall=%.2f)' % (self._hash_function, self._hash_length, self._recall)
+
+class ProjectionLSH(BaseANN):
+    def __init__(self, metric, params):
+        self._metric = metric
+        self._hash_function = params['hash_function']
+        self._repetitions = params['repetitions']
+        self._hash_length = params['hash_length']
+        self._block_size = params['block_size']
+
+    def fit(self, X):
+        self._graph = build_graph(
+            self._metric,
+            X,
+            self._count,
+            method = 'projection',
+            hash_function = self._hash_function,
+            repetitions = self._repetitions,
+            hash_length = self._hash_length,
+            block_size = self._block_size)
+
+    def query(self, idx, n):
+        return self._graph[idx]
+
+    def builds_graph(self):
+        return True
+
+    def set_count(self, count):
+        self._count = count
+
+    def __str__(self):
+        return 'ProjectionLSH(hash=%s, hash_length=%d, repetitions=%d, block_size=%d)' % (self._hash_function, self._hash_length, self._repetitions, self._block_size)
+
+
