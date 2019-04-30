416b9c565235c07440aefd1eca12ce5f57788f8e
Michael Vesterli <michael@vesterli.dk>
2019-04-30 21:42:42 +0200

    Update to new LSH graph algorithms

--- ann_benchmarks/algorithms/graph/puffinn.py
+++ ann_benchmarks/algorithms/graph/puffinn.py
@@ -5,9 +5,7 @@ from _puffinnwrapper import build_graph
 class PlainLSH(BaseANN):
     def __init__(self, metric, params):
         self._metric = metric
-        self._hash_function = params['hash_function']
-        self._recall = params['recall']
-        self._hash_length = params['hash_length']
+        self._params = params
 
     def fit(self, X):
         self._graph = build_graph(
@@ -15,9 +13,7 @@ class PlainLSH(BaseANN):
             X,
             self._count,
             method = 'plain',
-            hash_function = self._hash_function,
-            recall = self._recall,
-            hash_length = self._hash_length)
+            **params)
 
     def query(self, idx, n):
         return self._graph[idx]
@@ -29,15 +25,12 @@ class PlainLSH(BaseANN):
         self._count = count
 
     def __str__(self):
-        return 'PlainLSH(hash=%s, hash_length=%d, recall=%.2f)' % (self._hash_function, self._hash_length, self._recall)
+        return 'PlainLSH(params=%s)' % self._params
 
 class ProjectionLSH(BaseANN):
     def __init__(self, metric, params):
         self._metric = metric
-        self._hash_function = params['hash_function']
-        self._repetitions = params['repetitions']
-        self._hash_length = params['hash_length']
-        self._block_size = params['block_size']
+        self._params = params
 
     def fit(self, X):
         self._graph = build_graph(
@@ -45,10 +38,7 @@ class ProjectionLSH(BaseANN):
             X,
             self._count,
             method = 'projection',
-            hash_function = self._hash_function,
-            repetitions = self._repetitions,
-            hash_length = self._hash_length,
-            block_size = self._block_size)
+            **self._params)
 
     def query(self, idx, n):
         return self._graph[idx]
@@ -60,22 +50,20 @@ class ProjectionLSH(BaseANN):
         self._count = count
 
     def __str__(self):
-        return 'ProjectionLSH(hash=%s, hash_length=%d, repetitions=%d, block_size=%d)' % (self._hash_function, self._hash_length, self._repetitions, self._block_size)
+        return 'ProjectionLSH(params=%s)' % self._params
 
-class VariableHashLSH(BaseANN):
+class FixedBucketsLSH(BaseANN):
     def __init__(self, metric, params):
         self._metric = metric
-        self._hash_function = params['hash_function']
-        self._recall = params['recall']
+        self._params = params
 
     def fit(self, X):
         self._graph = build_graph(
             self._metric,
             X,
             self._count,
-            method = 'variable_hash',
-            hash_function = self._hash_function,
-            recall = self._recall)
+            method = 'fixed_buckets',
+            **self._params)
 
     def query(self, idx, n):
         return self._graph[idx]
@@ -87,5 +75,5 @@ class VariableHashLSH(BaseANN):
         self._count = count
 
     def __str__(self):
-        return 'VariableHashLSH(hash=%s, recall: %.2f)' % (self._hash_function, self._recall)
+        return 'VariableHashLSH(params=%s)' % self._params
 
