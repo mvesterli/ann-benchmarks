94c8480e8d1a74f516c60e8ba2d3ee72874db417
Michael Vesterli <michael@vesterli.dk>
2019-05-01 22:56:21 +0200

    Add puffinn nndescent

--- ann_benchmarks/algorithms/graph/puffinn.py
+++ ann_benchmarks/algorithms/graph/puffinn.py
@@ -77,3 +77,28 @@ class FixedBucketsLSH(BaseANN):
     def __str__(self):
         return 'VariableHashLSH(params=%s)' % self._params
 
+class NNDescent(BaseANN):
+    def __init__(self, metric, params):
+        self._metric = metric
+        self._params = params
+
+    def fit(self, X):
+        self._graph = build_graph(
+            self._metric,
+            X,
+            self._count,
+            method = 'nndescent',
+            **self._params)
+
+    def query(self, idx, n):
+        return self._graph[idx]
+
+    def builds_graph(self):
+        return True
+
+    def set_count(self, count):
+        self._count = count
+
+    def __str__(self):
+        return 'NNDescent(params=%s)' % self._params
+
