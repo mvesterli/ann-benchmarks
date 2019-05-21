991b8d95b948100fdb621b2a11061a286bd99fbe
Michael <miev@itu.dk>
2019-05-21 23:03:57 +0200

    Simplified puffinn graph runner

--- ann_benchmarks/algorithms/graph/puffinn.py
+++ ann_benchmarks/algorithms/graph/puffinn.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from ann_benchmarks.algorithms.base import BaseANN
 from _puffinnwrapper import build_graph
 
-class PlainLSH(BaseANN):
+class Puffinn(BaseANN):
     def __init__(self, metric, params):
         self._metric = metric
         self._params = params
@@ -12,7 +12,6 @@ class PlainLSH(BaseANN):
             self._metric,
             X,
             self._count,
-            method = 'plain',
             **self._params)
 
     def query(self, idx, n):
@@ -25,80 +24,4 @@ class PlainLSH(BaseANN):
         self._count = count
 
     def __str__(self):
-        return 'PlainLSH(params=%s)' % self._params
-
-class ProjectionLSH(BaseANN):
-    def __init__(self, metric, params):
-        self._metric = metric
-        self._params = params
-
-    def fit(self, X):
-        self._graph = build_graph(
-            self._metric,
-            X,
-            self._count,
-            method = 'projection',
-            **self._params)
-
-    def query(self, idx, n):
-        return self._graph[idx]
-
-    def builds_graph(self):
-        return True
-
-    def set_count(self, count):
-        self._count = count
-
-    def __str__(self):
-        return 'ProjectionLSH(params=%s)' % self._params
-
-class FixedBucketsLSH(BaseANN):
-    def __init__(self, metric, params):
-        self._metric = metric
-        self._params = params
-
-    def fit(self, X):
-        self._graph = build_graph(
-            self._metric,
-            X,
-            self._count,
-            method = 'fixed_buckets',
-            **self._params)
-
-    def query(self, idx, n):
-        return self._graph[idx]
-
-    def builds_graph(self):
-        return True
-
-    def set_count(self, count):
-        self._count = count
-
-    def __str__(self):
-        return 'VariableHashLSH(params=%s)' % self._params
-
-class NNDescent(BaseANN):
-    def __init__(self, metric, params):
-        self._metric = metric
-        self._params = params
-
-    def fit(self, X):
-        self._graph = build_graph(
-            self._metric,
-            X,
-            self._count,
-            method = 'nndescent',
-            **self._params)
-
-    def query(self, idx, n):
-        return self._graph[idx]
-
-    def builds_graph(self):
-        return True
-
-    def set_count(self, count):
-        self._count = count
-
-    def __str__(self):
-        return 'NNDescent(params=%s)' % self._params
-
+        return 'LSH(params=%s)' % self._params
